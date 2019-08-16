d065642bbc26347856cf04cbb3d9897469dfa7ee
Mahfuz <mahfuzcse12@gmail.com>
2019-08-16 15:34:27 +0600

    Logout

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -31,7 +31,7 @@
                   <button type="submit" class="btn btn-primary">Submit</button>
                </form>
            </div>
-         </div> 
+         </div>
     </div>
  </div>
   
