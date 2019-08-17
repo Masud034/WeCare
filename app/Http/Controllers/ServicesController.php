370f1deb6b189a3d339a3fae4db09634929dcf1b
Mahfuz <mahfuzcse12@gmail.com>
2019-08-17 19:49:38 +0600

    Services controller,blade updated

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -6,13 +6,16 @@ use Illuminate\Http\Request;
 
 class ServicesController extends Controller
 {
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('services');
-    }
+	public function index()
+	{
+		$serviceProviders=\DB::select('SELECT u.first_name,s.title,su.offerings,su.photo
+		from users as u join service_user as su
+		on su.user_id=u.id
+		join services as s
+		on s.id=su.service_id');
+
+		// dd($serviceProviders);
+
+		return view('services',compact('serviceProviders'));
+	}
 }
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,6 +1,22 @@
 @extends('master')
 @section('content')
-    <div class="container">
-    	<h1>Services</h1>
-    </div>
+<div class="container">
+	<div class="row">
+		@foreach($serviceProviders as $sp)
+		<div class="col-md-4" style="margin-bottom: 20px;">
+			<div class="card">
+			<img src="{{ asset(Storage::url($sp->photo)) }}" class="card-img-top" onerror="this.src='https://placehold.it/200x200'" style="height: 200px;" alt="Photo" class="img-thumbnail">
+			<div class="card-body">
+				<h5 class="card-title">Provider: {{ $sp->first_name }}</h5>
+				<p class="card-text">{{ $sp->offerings }}</p>
+				<p class="card-text text-right">
+					<small class="text-muted"><em>Category:</em></small>
+					<small class="text-success">{{ $sp->title }}</small>
+				</p>
+			</div>
+		</div>
+		</div>
+		@endforeach
+	</div>
+</div>
 @endsection
