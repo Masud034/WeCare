ac8b22169385699cbcf8640ea06dbad60280b9c0
Mahfuz <mahfuzcse12@gmail.com>
2019-08-26 15:56:07 +0600

    Migration update to bookings table

---
+++ database/migrations/2019_08_26_095310_add_more_columns_to_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMoreColumnsToBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->date('from_date')->after('number_of_days');
+            $table->date('to_date')->after('from_date');
+            $table->smallInteger('rating')->after('service_provider_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->dropColumn('from_date');
+            $table->dropColumn('to_date');
+            $table->dropColumn('rating');
+        });
+    }
+}
